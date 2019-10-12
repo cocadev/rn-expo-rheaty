466b5be20e9937a21be25116f6335a4f0e637041
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-10-12 20:16:06 +0800

    convert code to expo35

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Router from './src/router';
+import { Provider } from 'react-redux';
+import { configureStore } from './src/store';
 
 export default function App() {
   return (
